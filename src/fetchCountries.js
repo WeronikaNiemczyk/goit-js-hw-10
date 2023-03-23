08fa932d0345f0cffd4aa0467a72b6e88b626b1c
Weronika Niemczyk <weronika.tlusciak@gmail.com>
2023-03-23 18:17:28 +0100

    main part done

---
+++ src/fetchCountries.js
@@ -0,0 +1,46 @@
+import './css/styles.css';
+
+export const fetchCountries = name => {
+  const BASE_URL = 'https://restcountries.com/v3.1/name/';
+  const properties = 'fields=name,capital,population,flags,languages';
+  return fetch(`${BASE_URL}${name}?${properties}`)
+    .then(response => {
+      // console.log(response);
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      const countryData = response.json();
+      return countryData;
+      //   console.log('CountryData: ', countryData);
+    })
+    .then(receivedCountries => {
+      //   console.log('countries: ', receivedCountries);
+      return receivedCountries;
+    })
+    .catch(error => {
+      console.error('Problem:', error);
+    });
+};
+
+console.log(fetchCountries());
+
+// export const fetchCountries = name => {
+//   const basicURL = 'https://restcountries.com/v3.1/name/';
+//   const properties = 'fields=name,capital,population,flags,languages';
+//   console.log(basicURL)
+//   return fetch(`${basicURL}${name}?${properties}`)
+//     .then(response => {
+//       console.log(response);
+//   if (!response.ok) {
+//     throw new Error(response.status);
+//   }
+//   return response.json();
+// })
+// .then(countries => {
+//   console.log('countries: ', countries);
+//   return countries;
+// })
+// .catch(error => {
+//   console.error('Problem:', error);
+// });
+// };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,75 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './fetchCountries';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+
+const input = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+const cleanInput = clean => (clean.innerHTML = '');
+
+const inputHandler = event => {
+  const textInput = event.target.value.trim();
+  console.log('Text input: ', textInput);
+
+  if (!textInput) {
+    cleanInput(countryList);
+    cleanInput(countryInfo);
+    return;
+  }
+
+  fetchCountries(textInput)
+    .then(dataList => {
+      console.log('dataList', dataList);
+      if (dataList.length > 10) {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      }
+      showResults(dataList);
+    })
+    .catch(error => {
+      cleanInput(countryList);
+      cleanInput(countryInfo);
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+    });
+};
+
+const createList = dataList => {
+  return dataList
+    .map(
+      ({ name, flags }) =>
+        `<li><img src="${flags.svg}" width = "50" height = "50">${name.common}</li>`
+    )
+    .join('');
+};
+const createInfo = dataList => {
+  return dataList.map(
+    ({ name, capital, population, flags, languages }) =>
+      `<img src="${flags.svg}" width = "100" height = "100"><h1>${
+        name.common
+      }</h1>
+  <p><b>Capital:</b> ${capital}</p>
+  <p><b>Population:</b> ${population}</p>
+  <p><b>Languages:</b> ${Object.values(languages)}</p>`
+  );
+};
+const showResults = dataList => {
+  if (dataList.length === 1) {
+    cleanInput(countryList);
+    const ShowInfo = createInfo(dataList);
+    countryInfo.innerHTML = ShowInfo;
+    console.log(ShowInfo);
+  }
+  cleanInput(countryInfo);
+  const ShowList = createList(dataList);
+  countryList.innerHTML = ShowList;
+};
+
+// console.log(fetchCountries(textInput))
+// console.log('Inputhandler: ', inputHandler());
+
+input.addEventListener('input', debounce(inputHandler, DEBOUNCE_DELAY));
